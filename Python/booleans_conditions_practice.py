e9d8af46668516bbdc237b644b6cd094bbb08f3c
Sofia Kurochkina <sofikurochkina@gmail.com>
2020-06-25 10:37:11 +0500

    Booleans and conditions practice

--- Python/booleans_conditions_practice.py
+++ Python/booleans_conditions_practice.py
@@ -1,97 +1,86 @@
-# Booleans
-def can_run_for_president(age):
-    """Can someone of the given age run for president in the US?"""
-    # The US Constitution says you must be at least 35 years old
-    return age >=35
-print ("Can a 19-year-old run for president?",can_run_for_president(19))  
-print ("Can a 45-year-old run for president?",can_run_for_president(45)) 
-
-3.0 == 3
-#true
-
-'3' == 3
-#false
-
-#checking whether a number is odd or not
-def is_odd(n):
-    return (n%2) == 1
-print ("Is 100 odd?", is_odd(100))
-print ("Is -1 is odd?", is_odd(-1))
-# Is 100 odd? False
-# Is -1 odd? True
-
-#combining Boolean values 
-def can_run_for_president(age, is_natural_born_citizen):
-    """Can someone of the given age and citizenship status run for president in the US?"""
-    # The US Constitution says you must be a natural born citizen *and* at least 35 years old
-    return is_natural_born_citizen and (age>=35)
-
-print(can_run_for_president(19, True))
-print(can_run_for_president(55, False))
-print(can_run_for_president(55, True))
-
-#False
-#False
-#True
-
-# The order of operations 
-# and is evaluated before or 
-
-True or True and False
-#True 
-#True or True is True
-#True and False is False 
-#True or False is True
-
-prepared_for_weather = (
-    have_umbrella
-    or ((rain_level < 5 and have_hood)
-    or not (rain_level > 0 and is_workday))
-)
-
-#Conditional statements 
-# if, elif, else
-
-def inspect(x):
-    if x == 0:
-        print (x,"is zero")
-    elif x>0:
-        print (x, "is positive")
-    elif x<0:
-        print (x, "is negative")
+# 1
+def sign(x):
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
     else:
-        print (x, "is unlike anything I've ever seen...")
-inspect (0)
-inspect (-15)
+        return 0
 
-# 0 is zero
-# -15 is negative 
+# 2 
+def to_smash(total_candies):
+    """Return the number of leftover candies that must be smashed after distributing
+    the given number of candies evenly between 3 friends.
+    
+    >>> to_smash(91)
+    1
+    """
+    print("Splitting", total_candies, "candy" if total_candies == 1 else "candies")
+    return total_candies % 3
 
-def f(x):
-    if x>0:
-        print ("Only printed when x is positive; x=",x)
-        print ("Also onle printed when x is positive; x=",x)
-    print ("Always printed, regardless of x's value; x=",x)
-f(1)
-f(0)
+# 3
+def prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday):
+    return have_umbrella or rain_level < 5 and have_hood or (not rain_level > 0 and is_workday)
+have_umbrella = False
+rain_level = 0.00
+have_hood = False
+is_workday = False
+actual = prepared_for_weather(have_umbrella, rain_level, have_hood, is_workday)
 
-# Only printed when x is positive; x = 1
-# Also only printed when x is positive; x = 1
-# Always printed, regardless of x's value; x = 1
-# Always printed, regardless of x's value; x = 0
+# 4
+def is_negative(number):
+    if number <0: 
+        return True
+    else:
+        return False
 
-# Boolean conversion 
-print(bool(1)) # all numbers are treated as true, except 0
-print(bool(0))
-print(bool("asf"))  # all strings are treated as true, except the empty string ""
-print(bool(""))
+def concise_is_negative(number):
+    return number <0
 
-#True
-#False
-#True
-#False 
+# 5
+def onionless(ketchup, mustard, onion):
+    """Return whether the customer doesn't want onions.
+    """
+    return not onion
 
-if 0:
-    print(0)
-elif "spam":
-    print("spam")
+def wants_all_toppings(ketchup, mustard, onion):
+    """Return whether the customer wants "the works" (all 3 toppings)
+    """
+    return ketchup and mustard and onion
+
+def wants_plain_hotdog(ketchup, mustard, onion):
+    """Return whether the customer wants a plain hot dog with no toppings.
+    """
+    # return not ketchup and not mustard and not onion
+    return not (ketchup or mustard or onion)
+
+def exactly_one_sauce(ketchup, mustard, onion):
+        """Return whether the customer wants either ketchup or mustard, but not both.
+    (You may be familiar with this operation under the name "exclusive or")
+    """
+    return (ketchup and not mustard) or (mustard and not ketchup)
+
+# 6
+def exactly_one_topping(ketchup, mustard, onion):
+    """Return whether the customer wants exactly one of the three available toppings
+    on their hot dog.
+    """
+    # return (int(ketchup) + int(mustard) + int(onion)) == 1
+    # Python implicitly does the integer conversion
+    return (ketchup + mustard + onion) == 1
+
+# 7 blackjack ( aka twenty-one )
+def should_hit(player_total, dealer_total, player_aces):
+    """Return True if the player should hit (request another card) given the current game
+    state, or False if the player should stay. player_aces is the number of aces the player has.
+    """
+    if player_aces > 0:
+        return True if player_total <= 17 else False
+    else:
+        if player_total <= 17 and dealer_total >=7: 
+            return True 
+        elif dealer_total < 7: 
+            return player_total <= 17 
+        else: 
+            return False 
+    return False

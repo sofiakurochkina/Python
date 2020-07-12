a2f0cd443caa33962c3176648686aa0bbdf655ee
Sofia Kurochkina <sofikurochkina@gmail.com>
2020-07-12 18:08:20 +0500

    small projects

--- NumPy/demographic_data_analyzer.py
+++ NumPy/demographic_data_analyzer.py
@@ -18,8 +18,45 @@ def calculate_demographic_data(print_data=True):
 
     # with or without 'Bachelors','Masters', or 'Doctorate'
     higher_education=df.loc[df['education'].isin(['Bachelors','Masters','Doctorate'])]
-    lower_education=df.loc[~df['education'].isin(['Bachelor', 'Masters','Doctorate'])]
+    lower_education=df.loc[~df['education'].isin(['Bachelors', 'Masters','Doctorate'])]
 
     # percentage with salary > 50K
     higher_education_rich=round((higher_education['salary']=='>50K').sum()/len(higher_education)*100,1)
     lower_education_rich=round((lower_education['salary']=='>50K').sum()/len(lower_education)*100,1)
+
+    # What is the minimum number of hours a person works per week (hours-per-week feature)?
+    min_work_hours=df['hours-per-week'].min()
+
+    # What percentage of the people who work with the minimum number of hours per week have a salary of '>50K'
+    num_min_workers=df[df['hours-per-weeek'] == min_work_hours].shape[0]
+    rich_percentage=round((float(df[(df['hours-per-week'] == min_work_hours) & (df['salary']=='>50K')].shape[0])/num_min_workers)*100,1)
+
+    # What country has the highest percentage of poeple that earn > 50K?
+    highest_earning_country=''
+    highest_earning_country_percentage = 0
+    for (country), sub_df in df.groupby(
+        ['native-country']):
+        percentage=len(sub_df[(sub_df['salary']=='>50K')])/len(sub_df)
+        if highest_earning_country_percentage<percentage:
+            highest_earning_country_percentage=round(percentage,3)
+            highest_earning_country=country
+    highest_earning_country_percentage *=100
+
+    # Identify the most popular occupation for those who earn >50K in India. 
+    top_IN_occupation=df[(df['salary']=='>50K')&(df['native-country']=='India')]['occupation'].value_counts().keys()[0]
+
+    # DO NOT MODIFY BELOW THIS LINE
+
+  if print_data:
+    print("Number of each race:\n", race_count)
+    print("Average age of men:", average_age_men)
+    print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+    print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+    print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+    print(f"Min work time: {min_work_hours} hours/week")
+    print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+    print("Country with highest percentage of rich:", highest_earning_country)
+    print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
+    print("Top occupations in India:", top_IN_occupation)
+
+  return {'race_count': race_count, 'average_age_men': average_age_men, 'percentage_bachelors': percentage_bachelors, 'higher_education_rich': higher_education_rich, 'lower_education_rich': lower_education_rich, 'min_work_hours': min_work_hours, 'rich_percentage': rich_percentage, 'top_IN_occupation': top_IN_occupation}

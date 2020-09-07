b9260b00ee1d909c8f699971ba1956701e57d84f
Sofia Kurochkina <sofikurochkina@gmail.com>
2020-09-07 20:01:09 +0300

    Reverse Integer
    Given a 32-bit signed integer 
    Reverse digits of an integer

---
+++ Leetcode/reverse_integer.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def reverse(self,n):
+        y=str(abs(n))
+        y=y.strip()
+        y=y[::-1]
+        output=int(y)
+
+        if output >= 2^31 -1 or output <= 2^31:
+            return 0
+        elif n<0
+            return -1*output
+        else 
+            return output 
+        
+
+
